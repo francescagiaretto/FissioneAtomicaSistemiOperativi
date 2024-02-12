17e84b3713d674a1647a0a01c423c8d8a6867b7f
Francesca <francesca.giaretto@gmail.com>
2024-02-12 17:18:33 +0100

    Aggiunta controlli iniziali atomo, da perfezionare

--- atomo.c
+++ atomo.c
@@ -1,9 +1,13 @@
 #include "library.h"
 
 int main(int argc, char* argv[]){
+    if (getpid() == -1) { // controllo iniziale: se l'atomo non ha pid meltdown
+        printf("meltdown.\n");
+        exit(EXIT_FAILURE);
+    } else if (getpid() <= MIN_N_ATOMICO) { // controllo se il pid è inferiore al numero atomico minimo
+        // termino processo
+        // scorie++
+    }
 
-    printf("%d", atoi(argv[1]));
-
-    // controllo iniziale: se l'atomo non ha pid meltdown
     // scissione atomica: pid padre + pid figlio = pid originale padre
 }
