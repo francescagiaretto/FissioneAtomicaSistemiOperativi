6087bb912f26298f12e37d19b1bb2a90d7a044a9
Niccolo Faraca <niccolofaraca@gmail.com>
2024-02-18 14:17:30 +0100

    prove

--- atomo.c
+++ atomo.c
@@ -10,6 +10,7 @@ int main(int argc, char* argv[]){
         // termino processo
         // scorie++
     }
+    
     nipote1 = rand()% n_atomico_padre + 1;
     nipote2 = n_atomico_padre - nipote1;
 
@@ -36,6 +37,7 @@ int main(int argc, char* argv[]){
                 break;
 
                 default: // controlli il padre
+                    printf("sono pid: %d\n", getpid());
                     printf("Padre: %d, Nipote1: %d, Nipote2: %d\n", n_atomico_padre, nipote1, nipote2);
                 break;
             }
