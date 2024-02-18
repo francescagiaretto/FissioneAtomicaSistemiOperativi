ff6e16e948f13dc8991fc69a7657374f597e4e29
Niccolo Faraca <niccolofaraca@gmail.com>
2024-02-18 13:02:46 +0100

    struct nanosecondi + step_alimentatore

--- alimentatore.c
+++ alimentatore.c
@@ -1,5 +1,10 @@
 #include "library.h"
 
 int main() {
-    
+
+    // STRUCT per definire una sleep contata in nanosecondi, non possibile usando una sleep() normale.
+    struct timespec step_nanosec;
+    step_nanosec.tv_sec = 0;            
+    step_nanosec.tv_nsec = STEP_ALIMENTATORE;
+    nanosleep(&step_nanosec, NULL);
 }
