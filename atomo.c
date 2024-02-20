196949c50600fd37b31e84a06ab6932f0ef04c5b
Niccolo Faraca <niccolofaraca@gmail.com>
2024-02-20 10:42:05 +0100

    atomo

--- atomo.c
+++ atomo.c
@@ -1,7 +1,7 @@
 #include "library.h"
 
 int main(int argc, char* argv[]){
-    int nipote1 = 0; int nipote2= 0; int vec_nipoti[] = {nipote1,nipote2};
+    int figlio = 0; int vec_nipoti[] = {figlio};
     int n_atomico_padre = atoi(argv[1]); int EN_LIB = 0;
 
     char appoggio[3];
@@ -11,39 +11,33 @@ int main(int argc, char* argv[]){
         // scorie++         
     }  
 
-    nipote1 = rand()% n_atomico_padre + 1;
-    nipote2 = n_atomico_padre - nipote1;
+    int temp = n_atomico_padre;
+    n_atomico_padre = rand()% n_atomico_padre + 1;
+    figlio = temp - n_atomico_padre;
 
 
     // TODO gestire la fork quando lo richiede l'attivatore.
-    for (int i = 0 ; i < 2; i++) {
-
-        pid_t pid_atomi = fork();
-        
-        srand(getpid()); //*  meglio usare getpid piuttosto che time(NULL) perché time randomizza in base al tempo del programma
-                        //*  mentre così usa il pid che è sempre diverso, il tempo del programma invece è sempre lo stesso
-
-        sprintf(appoggio, "%d",vec_nipoti[i]);
-        char * vec_atomo[] = {"atomo", appoggio, NULL};
-
-        // TODO funzione energy() che incrementa l'energia liberata nelle statistiche del master
-            switch (pid_atomi)
-
-            {
-                case -1: // meltdown
-                    printf("Simulazione terminata: meltdown.");
-                    exit(EXIT_FAILURE);
-                break;
-                
-                case 0: // controlli il figlio
-                    if(execve("atomo", vec_atomo, NULL)==-1) {perror("Execve nipote"); exit(EXIT_FAILURE);} 
-                break;
-
-                default: // controlli il padre
-                int max_num = MAX(nipote1, nipote2);
-                EN_LIB = nipote1*nipote2 - max_num; // NECESSARIO FARLO ANCHE NEL NIPOTE?         
-                printf("Padre: %d, Nipote1: %d, Nipote2: %d\n", n_atomico_padre, nipote1, nipote2);
-                break;
-            }
-    }
+    pid_t pid_atomi = fork();
+    
+    srand(getpid()); //*  meglio usare getpid piuttosto che time(NULL) perché time randomizza in base al tempo del programma
+                    //*  mentre così usa il pid che è sempre diverso, il tempo del programma invece è sempre lo stesso
+    sprintf(appoggio, "%d",vec_nipoti);
+    char * vec_atomo[] = {"atomo", appoggio, NULL};
+    // TODO funzione energy() che incrementa l'energia liberata nelle statistiche del master
+        switch (pid_atomi)
+        {
+            case -1: // meltdown
+                printf("Simulazione terminata: meltdown.");
+                exit(EXIT_FAILURE);
+            break;
+            
+            case 0: // controlli il figlio
+                if(execve("atomo", vec_atomo, NULL)==-1) {perror("Execve nipote"); exit(EXIT_FAILURE);} 
+            break;
+            default: // controlli il padre
+            int max_num = MAX(figlio, n_atomico_padre);
+            EN_LIB = figlio*n_atomico_padre - max_num; // NECESSARIO FARLO ANCHE NEL NIPOTE?         
+            printf("Padre: %d, Figlio: %d\n", n_atomico_padre, figlio);
+            break;
+        }
 }
