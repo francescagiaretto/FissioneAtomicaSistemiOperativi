ead02f13e5c31f9756c26589de98ef1e7918d998
dadegio <davide.giordano77@edu.unito.it>
2024-03-07 12:23:52 +0100

    inizio attivatore

--- attivatore.c
+++ attivatore.c
@@ -6,13 +6,5 @@ int main(int argc, char* argv[]) {
 
     // semaforo che controlla n atomi da decidere (possono lavorare 3 atomi insieme)
     // in caso di riuscita execvesu atomo
-    
-    /**
-     *  Ogni STEP_ATTIVATORE mandi 
-     * un segnale ad atomo che controlla un semaforo per scindere
-     * la chiave per accedere a una sezione critica del semaforo
-     * 
-     * Iteri n volte e ogni volta controllata la condizione mandi un segnale come sopra ad atomo
-    */
 
 }
